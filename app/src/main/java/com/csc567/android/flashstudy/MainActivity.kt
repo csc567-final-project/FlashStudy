ec955ff17f927e3a5540cefe50672a86caf87221
Blake Engelbrecht <bengelbrecht95@gmail.com>
2021-04-16 14:32:24 -0500

    added cards to dashboard view. Linked the cards to their respective activities once clicked.

--- app/src/main/java/com/csc567/android/flashstudy/MainActivity.kt
+++ app/src/main/java/com/csc567/android/flashstudy/MainActivity.kt
@@ -9,6 +9,9 @@ import android.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.cardview.widget.CardView
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +27,29 @@ class MainActivity : AppCompatActivity() {
 
         val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
         val navView = findViewById<NavigationView>(R.id.nav_view)
+
+        //cards
+        val studyCard: CardView = findViewById(R.id.studyCard)
+        val quizCard: CardView = findViewById(R.id.quizCard)
+        val coursesCard: CardView = findViewById(R.id.coursesCard)
+        val scoresCard: CardView = findViewById(R.id.scoresCard)
+
+        studyCard.setOnClickListener {
+            val intent = Intent(this, FlashCardSetActivity::class.java)
+            startActivity(intent)
+        }
+        quizCard.setOnClickListener {
+            val intent = Intent(this, FlashCardSetActivity::class.java)
+            startActivity(intent)
+        }
+        coursesCard.setOnClickListener {
+            val intent = Intent(this, CourseActivity::class.java)
+            startActivity(intent)
+        }
+        scoresCard.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
         drawerLayout.addDrawerListener(toggle)
